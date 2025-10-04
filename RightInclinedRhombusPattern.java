eb2ee8a72d2c0373564c0bfdc46e807e3d60dcae
Onkar Ganjure <161829340+OnkarGanjure2003@users.noreply.github.com>
2025-10-04 15:24:06 +0530

    Add files via upload

---
+++ RightInclinedRhombusPattern.java
@@ -0,0 +1,18 @@
+package Lecture4;
+import java.util.Scanner;
+public class RightInclinedRhombusPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No of rows: ");
+        int row = sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=row-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=row;j++){
+                System.out.print("* " );
+            }
+            System.out.println();
+        }
+    }
+}
